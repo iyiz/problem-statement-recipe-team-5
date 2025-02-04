5c352030944d77168bafd0c13197c706508ea1e5
4fiq222 <23009096@myrp.edu.sg>
2025-02-04 10:21:34 +0800

    frotnend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+// // import { BrowserRouter, Routes, Route } from "react-router-dom";
+// // import Home from "./pages/Home";
+// import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
