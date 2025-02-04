b9d33d8a4460155f4197a668ecdec3f4f762ee33
Roderick8869 <23002007@myrp.edu.sg>
2025-02-04 11:08:22 +0800

    adadadad

--- frontend/src/hooks/useRecipeContext.js
+++ frontend/src/hooks/useRecipeContext.js
@@ -1,12 +1,12 @@
-import { WorkoutsContext } from '../context/RecipesContext'
-import { useContext } from 'react'
+import { RecipesContext } from '../context/RecipesContext';
+import { useContext } from 'react';
 
 export const useRecipesContext = () => {
-  const context = useContext(RecipesContext)
+  const context = useContext(RecipesContext);
 
   if (!context) {
-    throw Error('useRecipesContext must be used inside an RecipesContextProvider')
+    throw Error('useRecipesContext must be used inside a RecipesContextProvider');
   }
 
-  return context
-}
+  return context;
+};
