3d77170d880eb67319befeb4fada962c2229e2ae
marvell <23002203@myrp.edu.sg>
2025-02-04 15:14:38 +0800

    fixed blank screen, adjusted index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { AuthContext, AuthContextProvider } from './context/AuthContext';
+import { RecipesContextProvider } from './context/RecipeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <RecipesContextProvider>
+        <App />
+      </RecipesContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
