b71c755cbcd4485862b11bba4eb1b2c86a0b0cea
Roderick8869 <23002007@myrp.edu.sg>
2025-02-04 15:16:27 +0800

    dadddddddd

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,17 +1,21 @@
 import { useState } from "react"
+import {useLogin} from "../hooks/useLogin"
 
 const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const {login, error, isLoading} = useLogin()
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        console.log(email, password)
+
+        await login(email, password)
     }
 
     return (
         <form className="login" onSubmit={handleSubmit}>
             <h3>Login</h3>
+
             <label>Email:</label>
             <input
                 type="email"
@@ -24,7 +28,9 @@ const Login = () => {
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}
             />
-            <button>Login</button>
+
+            <button disabled= {isLoading} type = "submit">Login</button>
+            {error && <div className="error">{error}</div>}
         </form>
     )
 }
